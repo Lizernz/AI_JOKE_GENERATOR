fae809136b1ad7755dbf7a84d6bb5762595e1eeb
Lizernz <154061095+Lizernz@users.noreply.github.com>
2024-02-24 00:36:44 +0000

    added css to js

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-Let 
 
-
-
-
-
-function showAnswer(response) {
-    console.log(response);
-    alert(response.data.answer);
+function handleClick() {
+    let heading = document.querySelector("h1");
+    heading.classList.toggle = ("highlight");
 }
 
-let apiKey = "1de043300tfb174cf1a30ef403a9aobc";
-let context = "be polite and provide a very short answer";
-let prompt = "Who was the first Muganda man";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-
-console.log(apiUrl);
-axios.get(apiUrl).then(showAnswer)
+let buttonElement = document.querySelector("#special-button");
+console.log(buttonElement);
+buttonElement.addEventListener("click", handleClick);
