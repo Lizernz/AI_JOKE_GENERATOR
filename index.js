a4953929593057a1cd17941668e5e586e6bfc4d2
Lizernz <154061095+Lizernz@users.noreply.github.com>
2024-02-24 14:27:50 +0000

    drafting

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 
-function handleClick() {
-    let heading = document.querySelector("h1");
-    heading.classList.toggle = ("highlight");
+
+function showJoke(response) {
+    console.log(response);
+    alert(response.data.answer);
+
+    let jokeElement = document.querySelector("#joke");
+    jokeElement.innerHTML = response.data.answer;
+
+    new Typewriter("#joke", {
+
+        
+        strings: response.data.answer,
+        autoStart: true,
+        cursor: "",
+        delay: 10,
+      });
 }
+
+function handleClick() {
+    
+
+
+let apiKey = "1de043300tfb174cf1a30ef403a9aobc";
+let context = "be polite and provide a very short answer";
+let prompt = "Create a funny joke about comedians";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+axios.get(apiUrl).then(showJoke);
 
 let buttonElement = document.querySelector("#special-button");
 console.log(buttonElement);
-buttonElement.addEventListener("click", handleClick);
+buttonElement.addEventListener("click",handleClick);
+}
